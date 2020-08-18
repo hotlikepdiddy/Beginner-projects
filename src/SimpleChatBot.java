9ce11ac5d2c092fd9d614bd4617c50f3f2c3f760
hotlikepdiddy <69816990+hotlikepdiddy@users.noreply.github.com>
2020-08-18 13:55:02 +0300

    Just renaming

--- src/SimpleChatBot.java
+++ src/SimpleChatBot.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class temp {
+public class SimpleChatBot {
     final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
